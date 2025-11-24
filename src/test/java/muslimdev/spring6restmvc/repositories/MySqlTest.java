a523cf3583d121b571d52761e218fbeed67cd204
@muslimdev0211 <zoirjonabdulloh@gmail.com>
2025-11-24 16:45:19 +0500

    hfhvkjui

---
+++ src/test/java/muslimdev/spring6restmvc/repositories/MySqlTest.java
@@ -0,0 +1,39 @@
+package muslimdev.spring6restmvc.repositories;
+
+
+import muslimdev.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlTest {
+
+    @Container
+    static MySQLContainer<?> mySQLContainer =new MySQLContainer<>("mysql:8.0").withReuse(false);
+
+
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<Beer> beers = beerRepository.findAll();
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+}
